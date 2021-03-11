1c60c110706b02ec4fe6e67b555ce698162ca505
manoj <manojs121@rediffmail.com>
2021-03-11 18:14:15 +0530

    110121

---
+++ src/java/SubjectListServlet.java
@@ -0,0 +1,103 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SubjectListServlet extends HttpServlet {
+
+    PreparedStatement ps;
+    Connection con;
+
+    public void init() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ProjectData", "root", "root");
+            String sql = "SELECT distinct subject FROM quebank order by subject";
+            ps = con.prepareStatement(sql);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void destroy() {
+        try {
+            con.close();
+            System.out.println("Connection Closed.............");
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter out=response.getWriter();
+        
+        out.println("<html>");
+        out.println("<body>");
+        out.println("<h3>Subject-List</h3>");
+        out.println("<hr>");
+        try{
+            ResultSet rs=ps.executeQuery();
+            while(rs.next()){
+                String s=rs.getString(1);
+                out.println("<a href=\"DisplayQueSubjectWise\">");
+                out.println(s);
+                out.println("</a>");
+                out.println("<br>");
+            }
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        out.println("<hr>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- web/studentdashboard.jsp
+++ web/studentdashboard.jsp
@@ -3,13 +3,10 @@
         <h3>Student-Dashboard</h3>
         <hr>
         <pre>
-            //Links For Students Will Appear
-            => View Assignment
-            => Submit Assignment
-            => Change Profile
-            => ..
-            => ..
-            => Logout
+            <a href="SubjectListServlet">View-Questions-Subject-Wise</a>
+            <a href="">View-Questions-Date-Wise</a>
+            <a href="">Upload Solution</a>
+            <a href="index.jsp">Logout</a>
         </pre>
         <hr>
     </body>
